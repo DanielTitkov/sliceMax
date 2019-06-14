804706047f3b0465de5de08d72e059d7bac1a411
DanielTitkov <mytasmic@gmail.com>
2019-06-14 13:01:55 +0300

    working version

--- sliceMax.go
+++ sliceMax.go
@@ -7,31 +7,34 @@ import (
 )
 
 
-// switch reflect.TypeOf(t).Kind() {
-// case reflect.Slice:
-//     s := reflect.ValueOf(t)
-//
-//     for i := 0; i < s.Len(); i++ {
-//         fmt.Println(s.Index(i))
-//     }
-// }
-//
-// https://stackoverflow.com/questions/14025833/range-over-interface-which-stores-a-slice
-
-func max(inp interface{}, less func(i, j int) bool) {
+func maxIndex(inp interface{}, less func(i, j int) bool) (int, error) {
     switch reflect.TypeOf(inp).Kind() {
     case reflect.Slice:
         slice := reflect.ValueOf(inp)
-        for i := 0; i < slice.Len(); i++ {
-
+        if slice.Len() > 0 {
+            maxIndex := 0
+            for i := 0; i < slice.Len(); i++ {
+                if less(i, maxIndex) {
+                    maxIndex = i
+                }
+            }
+            return maxIndex, nil
         }
+        return 0, fmt.Errorf("Input slice is empty")
     default:
-        log.Errorf("Slice is not valid")
+        return 0, fmt.Errorf("Input is not a valid slice")
     }
 }
 
 
 func main() {
-    a := []int{4, 5, 2, 7, 2, 5}
-    max(a, )
+    a := []int{4, 5, 2, 7, 2, 5, 115, 7, 0, 22, 2, 2}
+    fmt.Println("Input data:", "\n", a)
+    i, err := maxIndex(a, func(i, j int) bool {
+        return a[i] > a[j]
+    })
+    if err == nil {
+        fmt.Println("Max index is:", i)
+        fmt.Println("Max value is:", a[i])
+    }
 }
--- sliceMax_test.go
+++ sliceMax_test.go
@@ -1,4 +1,142 @@
 package main
 
 
-import "testing"
+import (
+    "testing"
+    "errors"
+)
+
+
+func TestIntSlice(t *testing.T) {
+    t.Log("Searching index of max value in slice of ints")
+    a := []int{4, 5, 2, 7, 2, 5}
+    expected := 3
+    var expected_err error
+    res, err := maxIndex(a, func(i, j int) bool {
+        return a[i] > a[j]
+    })
+
+    if res != expected || err != expected_err {
+         t.Errorf("Expected %d, got %d, expected error %v, got %v", expected, res, expected_err, err)
+    }
+}
+
+
+func TestStringSlice(t *testing.T) {
+    t.Log("Searching index of max (by length) value in slice of strings")
+    a := []string{"m", "qoo", "foo", "oy", "bararbaram", "ouk"}
+    expected := 4
+    var expected_err error
+    res, err := maxIndex(a, func(i, j int) bool {
+        return len(a[i]) > len(a[j])
+    })
+
+    if res != expected || err != expected_err {
+         t.Errorf("Expected %d, got %d, expected error %v, got %v", expected, res, expected_err, err)
+    }
+}
+
+
+func TestStructSlice(t *testing.T) {
+    t.Log("Searching index of max value in slice of structs")
+
+    type Dude struct {
+        name string
+        height int
+        width int
+    }
+
+    a := []Dude{
+        {"Fatty", 100, 190},
+        {"Bill", 10, 14},
+        {"Arrrrrrasss", 0, 99900229},
+        {"Quaky", 54, 23},
+    }
+
+    expected := 0
+    var expected_err error
+    res, err := maxIndex(a, func(i, j int) bool {
+        dudeMetricI := len(a[i].name) * a[i].height * a[i].width // could have created method
+        dudeMetricJ := len(a[j].name) * a[j].height * a[j].width // but why bother
+        return dudeMetricI > dudeMetricJ
+    })
+
+    if res != expected || err != expected_err {
+         t.Errorf("Expected %d, got %d, expected error %v, got %v", expected, res, expected_err, err)
+    }
+}
+
+
+func TestEqualElements(t *testing.T) {
+    t.Log("Trying to send in slice with all equal elements. First should be selected")
+    a := []int{99, 99, 99, 99, 99, 99, 99, 99}
+    expected := 0
+    var expected_err error
+    res, err := maxIndex(a, func(i, j int) bool {
+        return a[i] > a[j]
+    })
+
+    if res != expected || err != expected_err {
+         t.Errorf("Expected %d, got %d, expected error %v, got %v", expected, res, expected_err, err)
+    }
+}
+
+
+func TestOneElement(t *testing.T) {
+    t.Log("Trying to send in slice with one element")
+    a := []int{42}
+    expected := 0
+    var expected_err error
+    res, err := maxIndex(a, func(i, j int) bool {
+        return a[i] > a[j]
+    })
+
+    if res != expected || err != expected_err {
+         t.Errorf("Expected %d, got %d, expected error %v, got %v", expected, res, expected_err, err)
+    }
+}
+
+
+func TestEmptySlice(t *testing.T) {
+    t.Log("Trying to send in empty slice")
+    a := []int{}
+    expected := 0
+    expected_err := errors.New("Input slice is empty")
+    res, err := maxIndex(a, func(i, j int) bool {
+        return a[i] > a[j]
+    })
+
+    if res != expected || err.Error() != expected_err.Error() {
+         t.Errorf("Expected %d, got %d, expected error %v, got %v", expected, res, expected_err, err)
+    }
+}
+
+
+func TestNilSlice(t *testing.T) {
+    t.Log("Trying to send in nil slice")
+    var a []int
+    expected := 0
+    expected_err := errors.New("Input slice is empty")
+    res, err := maxIndex(a, func(i, j int) bool {
+        return a[i] > a[j]
+    })
+
+    if res != expected || err.Error() != expected_err.Error() {
+         t.Errorf("Expected %d, got %d, expected error %v, got %v", expected, res, expected_err, err)
+    }
+}
+
+
+func TestInvalidSlice(t *testing.T) {
+    t.Log("Trying to send in invalid data")
+    a := "Not today!"
+    expected := 0
+    expected_err := errors.New("Input is not a valid slice")
+    res, err := maxIndex(a, func(i, j int) bool {
+        return a[i] > a[j]
+    })
+
+    if res != expected || err.Error() != expected_err.Error() {
+         t.Errorf("Expected %d, got %d, expected error %v, got %v", expected, res, expected_err, err)
+    }
+}
